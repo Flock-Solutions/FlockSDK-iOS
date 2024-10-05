929717337ed7db95a11fa3c9edd25ae242870615
Hoa Nguyen <43334675+hoadnguyen@users.noreply.github.com>
2024-10-04 20:35:42 -0700

    Fix

--- Sources/FlockSDK/Views/WebViewController.swift
+++ Sources/FlockSDK/Views/WebViewController.swift
@@ -10,20 +10,16 @@ import WebKit
 
 @available(iOS 13.0, *)
 class WebViewController: UIViewController, WKNavigationDelegate {
+    private var webView: WKWebView!
     private let url: URL
-    private var webView: WKWebView?
-    private var closeButton: UIButton?
-    private var shareButton: UIButton?
-    private var progressView: UIProgressView?
+    private var closeButton: UIButton!
+    private var shareButton: UIButton!
+    private var progressView: UIProgressView!
     private var progressObservation: NSKeyValueObservation?
     
     init(url: URL) {
         self.url = url
         super.init(nibName: nil, bundle: nil)
-        webView = setupWebView()
-        closeButton = setupCloseButton()
-        progressView = setupProgressView()
-        shareButton = setupShareButton()
     }
     
     required init?(coder: NSCoder) {
@@ -32,45 +28,31 @@ class WebViewController: UIViewController, WKNavigationDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        loadWebView()
-        loadCloseButton()
-        loadShareButton()
-        loadProgressView()
+        
+        setupWebView()
+        setupCloseButton()
+        setupProgressView()
     }
     
-    /*
-     WEB VIEW
-     */
-    private func setupWebView() -> WKWebView {
+    private func setupWebView() {
         let webConfiguration = WKWebViewConfiguration()
-        let webView = WKWebView(frame: view.bounds, configuration: webConfiguration)
+        webView = WKWebView(frame: view.bounds, configuration: webConfiguration)
+        webView.backgroundColor = .black
         webView.navigationDelegate = self
         webView.allowsBackForwardNavigationGestures = true
         webView.translatesAutoresizingMaskIntoConstraints = false
         webView.load(URLRequest(url: url))
         
-        return webView
+        NSLayoutConstraint.activate([
+            webView.topAnchor.constraint(equalTo: view.topAnchor),
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
     
-    private func loadWebView() {
-        if let webView = webView {
-            webView.frame = view.bounds
-            view.addSubview(webView)
-            NSLayoutConstraint.activate([
-                webView.topAnchor.constraint(equalTo: view.topAnchor),
-                webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-                webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-                webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-            ])
-        }
-    }
-    
-    /**
-     BUTTONS
-     */
-    private func setupCloseButton() -> UIButton {
-        let closeButton = UIButton(type: .roundedRect)
+    private func setupCloseButton() {
+        closeButton = UIButton(type: .roundedRect)
         closeButton.setTitle("✕", for: .normal)
         closeButton.titleLabel?.font = UIFont.systemFont(ofSize: 20)
         closeButton.setTitleColor(.white, for: .normal)
@@ -80,68 +62,49 @@ class WebViewController: UIViewController, WKNavigationDelegate {
         closeButton.setTitleColor(.white, for: .normal)
         closeButton.addTarget(self, action: #selector(closeTapped), for: .touchUpInside)
         closeButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(closeButton)
         
-        return closeButton
+        NSLayoutConstraint.activate([
+            closeButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            closeButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            closeButton.widthAnchor.constraint(equalToConstant: 32),
+            closeButton.heightAnchor.constraint(equalToConstant: 32)
+        ])
     }
     
-    private func loadCloseButton() {
-        if  let closeButton = closeButton {
-            view.addSubview(closeButton)
-            NSLayoutConstraint.activate([
-                closeButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
-                closeButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
-                closeButton.widthAnchor.constraint(equalToConstant: 32),
-                closeButton.heightAnchor.constraint(equalToConstant: 32)
-            ])
-        }
-    }
-    
-    private func setupShareButton() -> UIButton {
-        let shareButton = UIButton(type: .roundedRect)
-        let image = UIImage(named: "share-icon", in: .module, with: nil)
-        shareButton.setImage(image, for: .normal)
-        shareButton.tintColor = .white
+    private func setupShareButton() {
+        shareButton = UIButton(type: .roundedRect)
+        
+        let shareImg = UIImage(named: "share-icon", in: .module, with: nil)!
+        shareButton.setImage(shareImg, for: .normal)
+        shareButton.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        shareButton.setTitleColor(.white, for: .normal)
         shareButton.clipsToBounds = true
         shareButton.layer.cornerRadius = 16
         shareButton.backgroundColor = UIColor.black.withAlphaComponent(0.6)
         shareButton.setTitleColor(.white, for: .normal)
         shareButton.addTarget(self, action: #selector(closeTapped), for: .touchUpInside)
         shareButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(shareButton)
         
-        return shareButton
+        NSLayoutConstraint.activate([
+            shareButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            shareButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            shareButton.widthAnchor.constraint(equalToConstant: 32),
+            shareButton.heightAnchor.constraint(equalToConstant: 32)
+        ])
     }
     
-    private func loadShareButton() {
-        if let shareButton = shareButton {
-            view.addSubview(shareButton)
-            NSLayoutConstraint.activate([
-                shareButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
-                shareButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: -16),
-                shareButton.widthAnchor.constraint(equalToConstant: 32),
-                shareButton.heightAnchor.constraint(equalToConstant: 32)
-            ])
-        }
-    }
-    
-    /**
-     PROGRESS BAR
-     */
-    private func setupProgressView() -> UIProgressView {
-        let progressView = UIProgressView(progressViewStyle: .default)
+    private func setupProgressView() {
+        progressView = UIProgressView(progressViewStyle: .default)
         progressView.translatesAutoresizingMaskIntoConstraints = false
-    
-        return progressView
-    }
-    
-    private func loadProgressView() {
-        if let progressView = progressView {
-            view.addSubview(progressView)
-            NSLayoutConstraint.activate([
-                progressView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-                progressView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-                progressView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            ])
-        }
+        view.addSubview(progressView)
+        
+        NSLayoutConstraint.activate([
+            progressView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            progressView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            progressView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
     }
     
     @objc func closeTapped() {
@@ -150,6 +113,11 @@ class WebViewController: UIViewController, WKNavigationDelegate {
         } else {
             dismiss(animated: true, completion: nil)
         }
+    }
+    
+    private func updateProgress() {
+        progressView.progress = Float(webView.estimatedProgress)
+        progressView.isHidden = webView.estimatedProgress == 1
     }
     
     deinit {
