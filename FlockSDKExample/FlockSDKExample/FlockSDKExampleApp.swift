97419d2111b346a09804838b9ea90683cd4eb934
Hoa Nguyen <43334675+hoadnguyen@users.noreply.github.com>
2024-10-05 00:37:21 -0700

    Fix identify script

--- FlockSDKExample/FlockSDKExample/FlockSDKExampleApp.swift
+++ FlockSDKExample/FlockSDKExample/FlockSDKExampleApp.swift
@@ -11,7 +11,9 @@ import FlockSDK
 @main
 struct FlockSDKExampleApp: App {
     init() {
-        Flock.configure (publicAccessKey: "6d004555db663cd2b4d2a9594fa80a1a", campaignId: "campaign_01j7kgkcn1eaqtg5vv9bac2xf9", overrideApiURL: "http://localhost:3000")
+        Flock.configure(publicAccessKey: "6d004555db663cd2b4d2a9594fa80a1a", campaignId: "campaign_01j7kgkcn1eaqtg5vv9bac2xf9", overrideApiURL: "http://localhost:3000")
+        
+        Flock.shared.identify(externalUserId: "user_01j7kgkcn1eaqtg5vv9bac2xf9", email: "user@example.com", name: "User Name")
     }
     
     var body: some Scene {
--- Sources/FlockSDK/API/APIClient.swift
+++ Sources/FlockSDK/API/APIClient.swift
@@ -5,11 +5,16 @@
 //  Created by Hoa Nguyen on 2024-10-03.
 //
 import Foundation
+import OSLog
 
 @available(iOS 14.0, *)
 struct APIClient {
     private let urlBuilder: URLBuilder
     private let requestBuilder: RequestBuilder
+    private let logger = Logger(
+        subsystem: Bundle.main.bundleIdentifier!,
+        category: String(describing: Flock.self)
+    )
     
     init(apiKey: String, baseURL: String?) {
         self.urlBuilder = URLBuilder(baseURL: baseURL)
@@ -36,13 +41,18 @@ struct APIClient {
     func identify(identifyRequest: IdentifyRequest) async throws -> Customer {
         let url = try self.urlBuilder.build(path: "/customers/identify")
 
-        let request = self.requestBuilder.build(url: url, method: .post)
+        var request = self.requestBuilder.build(url: url, method: .post)
         
         let jsonRequest = try JSONEncoder().encode(identifyRequest)
+        request.httpBody = jsonRequest
         
         let (data, response) = try await URLSession.shared.data(for: request)
 
         guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+            if let errorResponse = response as? HTTPURLResponse {
+                logger.error("Error identifying customer: \(errorResponse)")
+            }
+            
             throw URLError(.badServerResponse)
         }
         
