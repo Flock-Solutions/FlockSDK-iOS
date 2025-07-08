ea0fe1f1f75fd6aa5580774ef3042ab0d60bf71f
Hoa Nguyen <43334675+hoadnguyen@users.noreply.github.com>
2025-07-07 22:27:12 -0700

    Add top margin

--- Sources/FlockSDK/Views/WebViewController.swift
+++ Sources/FlockSDK/Views/WebViewController.swift
@@ -116,7 +116,7 @@ public class WebViewController: UIViewController, WKNavigationDelegate, WKScript
         view.addSubview(webView)
 
         NSLayoutConstraint.activate([
-            webView.topAnchor.constraint(equalTo: view.topAnchor),
+            webView.topAnchor.constraint(equalTo: view.topAnchor, constant: 16),
             webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
